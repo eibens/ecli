01aba5cbeb17fb3c381ee2012fe1dce8a29d90f3
eibens <l.eibensteiner@gmail.com>
2021-06-21 17:38:27 +0200

    feat(help): add links section

--- help.ts
+++ help.ts
@@ -1,13 +1,15 @@
 import { Node } from "https://deno.land/x/ansiml@v0.0.3/mod.ts";
 import { Options } from "./options.ts";
 import { Permissions } from "./permissions.ts";
-import { Blocks, Heading, Sections } from "./text.ts";
+import { Blocks, Definition, Heading, Sections, Url } from "./text.ts";
 import { Usage } from "./usage.ts";
+import emoji from "./emoji.ts";
 
 export type Help = {
   icon: Node;
   name: string;
   description: string;
+  repository?: string;
   usage?: Usage;
   permissions?: Permissions;
   options?: Options;
@@ -21,10 +23,20 @@ export function Help(opts: Help): Node {
   const usage = opts.usage ? [Usage(opts.usage)] : [];
   const permissions = opts.permissions ? [Permissions(opts.permissions)] : [];
   const options = opts.options ? [Options(opts.options)] : [];
+  const links = opts.repository
+    ? [Blocks(
+      Heading(emoji.globeWithMeridians, "Links"),
+      Definition(
+        Url(opts.repository),
+        "Source code repository.",
+      ),
+    )]
+    : [];
   return Sections(
     title,
     ...usage,
     ...permissions,
     ...options,
+    ...links,
   );
 }
